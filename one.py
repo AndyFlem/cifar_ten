e0594ad62aeee9d9ffd8e9158e335c68ffad116c
Andy Fleming <andy@ulendo.com>
2018-05-02 09:35:39 +0200

    commit

--- one.py
+++ one.py
@@ -1,1 +1,14 @@
 import torch
+
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+cifar=unpickle("C:\\Users\\Andy\\Documents\\ML\\cifar-10-batches-py\\data_batch_1")
+
+print(cifar.keys())
+print(cifar[b'batch_label'].decode('utf-8'))
+
